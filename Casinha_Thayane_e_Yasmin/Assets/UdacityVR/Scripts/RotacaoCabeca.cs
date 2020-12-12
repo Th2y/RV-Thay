ca87c21d1f14c580345ab1a998babfd03d8456f7
Thayane <thayane.carvalho@bluewolfbr.com>
2020-12-12 18:18:26 -0300

    Completa

---
+++ Casinha_Thayane_e_Yasmin/Assets/UdacityVR/Scripts/RotacaoCabeca.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacaoCabeca : MonoBehaviour {
+
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+	
+	void Update () {
+		Quaternion att = Input.gyro.attitude;
+		att = Quaternion.Euler(90f, 0f, 0f) * new Quaternion(att.x, att.y, -att.z, -att.w);
+		transform.rotation = att;
+	}
+}
